a59a31e5ef3e485ee9d4b7fb36d2df40462fcf06
root <root@qq.com>
2025-11-23 13:44:09 +0800

    新增学习动画代码

--- refine-domain/src/main/java/com/achobeta/domain/overview/adapter/repository/IStudyOverviewRepository.java
+++ refine-domain/src/main/java/com/achobeta/domain/overview/adapter/repository/IStudyOverviewRepository.java
@@ -4,10 +4,18 @@ import com.achobeta.domain.overview.model.valobj.StudyOverviewVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+
 @Mapper
 public interface IStudyOverviewRepository {
 
     @Select("select questions_num, review_rate, hard_questions, study_time" +
             " from userdata where user_id = #{userId}")
     StudyOverviewVO queryStudyOverview(String userId);
+
+    @Select("select count(*) from mistakequestion where user_id = #{userId} and create_time > #{localDateTime}")
+    Integer countByUserIdAndCreateTimeAfter(String userId, LocalDateTime localDateTime);
+
+    @Select("select count(*) from mistakequestion where user_id = #{userId} and update_time between #{localDateTime} and #{localDateTime1}")
+    Integer countByUserIdAndUpdateTimeBetween(String userId, LocalDateTime localDateTime, LocalDateTime localDateTime1);
 }
--- refine-domain/src/main/java/com/achobeta/domain/overview/service/ILearningOverviewService.java
+++ refine-domain/src/main/java/com/achobeta/domain/overview/service/ILearningOverviewService.java
@@ -1,5 +1,6 @@
 package com.achobeta.domain.overview.service;
 
+import com.achobeta.domain.overview.model.valobj.LearningDynamicVO;
 import com.achobeta.domain.overview.model.valobj.StudyOverviewVO;
 
 public interface ILearningOverviewService {
@@ -10,4 +11,12 @@ public interface ILearningOverviewService {
      * @return 学习概览
      */
     StudyOverviewVO getOverview(String userId);
+
+    /**
+     * 获取学习动态
+     *
+     * @param userId 用户id
+     * @return 学习动态
+     */
+    LearningDynamicVO getStudyDynamic(String userId);
 }
