384cd2de390af98cfac1b8b5e8991cf19fdc1db1
malog <2153315236@qq.com>
2025-11-26 19:03:29 +0800

    fix: 解决依赖冲突问题

--- refine-app/src/main/java/com/achobeta/config/AiConfig.java
+++ refine-app/src/main/java/com/achobeta/config/AiConfig.java
@@ -1,24 +1,24 @@
-package com.achobeta.config;
-
-import com.achobeta.domain.aisuggession.service.ConsultantService;
-import dev.langchain4j.model.chat.ChatModel;
-import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.service.AiServices;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-@Configuration
-public class AiConfig {
-    @Autowired
-    private OpenAiChatModel model;
-    @Autowired
-    private ChatModel chatModel;
-
-    @Bean
-    public ConsultantService consultantService() {
-        return AiServices.builder(ConsultantService.class)
-                .chatModel(chatModel)
-                //.chatModel(model)
-                .build();
-    }
-}
+//package com.achobeta.config;
+//
+//import com.achobeta.domain.aisuggession.service.ConsultantService;
+//import dev.langchain4j.model.chat.ChatModel;
+//import dev.langchain4j.model.openai.OpenAiChatModel;
+//import dev.langchain4j.service.AiServices;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//@Configuration
+//public class AiConfig {
+//    @Autowired
+//    private OpenAiChatModel model;
+//    @Autowired
+//    private ChatModel chatModel;
+//
+//    @Bean
+//    public ConsultantService consultantService() {
+//        return AiServices.builder(ConsultantService.class)
+//                .chatModel(chatModel)
+//                //.chatModel(model)
+//                .build();
+//    }
+//}
