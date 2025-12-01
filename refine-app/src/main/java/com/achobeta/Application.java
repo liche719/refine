2b41e1f9ca1bcbd45af24119fa9d29040fae8486
root <root@qq.com>
2025-12-01 17:06:44 +0800

    fix:添加定时任务启动注解

--- refine-app/src/main/java/com/achobeta/Application.java
+++ refine-app/src/main/java/com/achobeta/Application.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {
     "com.achobeta",
     "com.achobeta.infrastructure"
 })
 @Configurable
+@EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Application {
 
